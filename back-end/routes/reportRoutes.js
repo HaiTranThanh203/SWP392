3963afe93c823f6e5ba3948f661246b5d75e3f29
Tran Thanh Hai <gtvvjetnam@gmail.com>
2025-02-15 17:31:15 +0700

    Add pagination and filtering to report retrieval; implement report statistics and post deactivation functionality

--- back-end/routes/reportRoutes.js
+++ back-end/routes/reportRoutes.js
@@ -4,6 +4,11 @@ const router = express.Router();
 
 router
   .route('/')
-  .get(reportController.getReportById); // Lấy tất cả báo cáo với phân trang
-  
+  .get(reportController.getAllReportsPaginate); // Lấy tất cả báo cáo với phân trang
+  router.get('/stats', reportController.getReportStats); // Lấy thống kê báo cáo
+
+// Route để vô hiệu hóa bài viết được báo cáo
+router.patch('/deactivate-report-post/:postId', reportController.deactivateReportedPost);
+router.post('/', reportController.createNewReport);
+router.patch('/:reportId', reportController.getReportById);
 module.exports = router;
